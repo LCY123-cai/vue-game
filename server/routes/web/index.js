05bfc8aa95cf0308bb1497d7e98afb05b146b2d2
LCY123-cai <2592186051@qq.com>
2020-05-02 15:54:47 +0800

    5.2---第三章完结2

--- server/routes/web/index.js
+++ server/routes/web/index.js
@@ -141,7 +141,7 @@ router.get('/articles/:id', async (req, res) => {
   router.get('/heroes/:id', async (req, res) => {
      const data = await Hero
         .findById(req.params.id)
-        .populate('categories')
+        .populate('categories items1 items2 partners.hero')
         .lean()
      res.send(data)
   })
